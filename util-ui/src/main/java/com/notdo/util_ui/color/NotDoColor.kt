17c2b5c00684297569601b49772242560e41467f
khs3994 <z01039948003@gmail.com>
2023-03-30 16:18:56 +0900

    :sparkles: :: Color Guide 작성

---
+++ util-ui/src/main/java/com/notdo/util_ui/color/NotDoColor.kt
@@ -0,0 +1,25 @@
+package com.notdo.util_ui.color
+
+import androidx.compose.ui.graphics.Color
+
+object NotDoColor {
+    val Black = Color(0xFF000000)
+    val White = Color(0xFFFFFFFF)
+
+    val Gray500 = Color(0xFF595959)
+    val Gray400 = Color(0xFF808080)
+    val Gray300 = Color(0xFFB3B3B3)
+    val Gray200 = Color(0xFFD3D3D3)
+    val Gray100 = Color(0xFFE5E5E5)
+
+    val Error = Color(0xFFB5B5B5)
+    val DimedLayer = Color(0x66000000)
+
+    object NotDo {
+        val Angry = Color(0xFFE76161)
+        val Happy = Color(0xFFFCD859)
+        val Normal = Color(0xFF83DE81)
+        val Sad = Color(0xFF558DE2)
+        val Gray = Color(0xFFB5B5B5)
+    }
+}
