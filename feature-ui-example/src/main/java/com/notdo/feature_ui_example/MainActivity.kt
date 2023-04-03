b03675cc85718c5f0775162ac6c99213e630e86e
khs3994 <z01039948003@gmail.com>
2023-04-03 17:25:20 +0900

    :sparkles: :: button component 추가

--- feature-ui-example/src/main/java/com/notdo/feature_ui_example/MainActivity.kt
+++ feature-ui-example/src/main/java/com/notdo/feature_ui_example/MainActivity.kt
@@ -5,22 +5,24 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import com.notdo.feature_ui_example.ui.theme.NotDoAosTheme
+import com.notdo.util_ui.component.NotDoButton
 import com.notdo.util_ui.font.NotDoFont
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            NotDoFont.Body(text = "이것은 바디 입니다!!", modifier = Modifier.clickable { Log.d("dd","ddd") }, fontWeight = FontWeight.SemiBold)
         }
     }
 }
