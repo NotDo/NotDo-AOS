f75564e249bf922d447b7d9b03a08d8871b556f4
khs3994 <z01039948003@gmail.com>
2023-04-03 21:01:48 +0900

    :sparkles: :: topbar 3개 모두 추가

--- feature-ui-example/src/main/java/com/notdo/feature_ui_example/MainActivity.kt
+++ feature-ui-example/src/main/java/com/notdo/feature_ui_example/MainActivity.kt
@@ -1,23 +1,10 @@
 package com.notdo.feature_ui_example
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
-import com.notdo.feature_ui_example.ui.theme.NotDoAosTheme
-import com.notdo.util_ui.component.NotDoButton
-import com.notdo.util_ui.font.NotDoFont
+import com.notdo.util_ui.component.DiaryHeader
+import com.notdo.util_ui.component.ScrollHeader
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
