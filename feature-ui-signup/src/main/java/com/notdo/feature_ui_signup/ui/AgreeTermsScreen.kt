a8e8bc4474cbb460f6de830e1df38bd1a04d674b
khs3994 <z01039948003@gmail.com>
2023-04-13 22:40:37 +0900

    :fire: :: 안쓰는 임포트 삭제

--- feature-ui-signup/src/main/java/com/notdo/feature_ui_signup/ui/AgreeTermsScreen.kt
+++ feature-ui-signup/src/main/java/com/notdo/feature_ui_signup/ui/AgreeTermsScreen.kt
@@ -1,6 +1,5 @@
 package com.notdo.feature_ui_signup.ui
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
