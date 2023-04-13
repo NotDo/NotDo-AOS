11ea89bacbb69af921f11704d3c9e8eeded30802
khs3994 <z01039948003@gmail.com>
2023-04-14 00:56:30 +0900

    :lipstick: :: 아메일 입력 페이지 완성

--- feature-ui-signup/src/main/java/com/notdo/feature_ui_signup/ui/EmailInputScreen.kt
+++ feature-ui-signup/src/main/java/com/notdo/feature_ui_signup/ui/EmailInputScreen.kt
@@ -7,7 +7,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.notdo.navigator.NotDoRoutes
+import com.notdo.util_kotlin.format.isEmailPattern
 import com.notdo.util_ui.component.GreetingsNotDoView
 import com.notdo.util_ui.component.NormalHeader
 import com.notdo.util_ui.component.NotDoButton
@@ -21,6 +21,10 @@ fun EmailInputScreen(navController: NavController) {
         mutableStateOf("")
     }
 
+    var isError by remember {
+        mutableStateOf(false)
+    }
+
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
@@ -31,28 +35,38 @@ fun EmailInputScreen(navController: NavController) {
         Spacer(modifier = Modifier.fillMaxHeight(0.05f))
         GreetingsNotDoView(text = "낫두가 \n처음이시군요?")
         Spacer(modifier = Modifier.fillMaxHeight(0.05f))
-        NotDoFont.Caption2(
-            text = "이메일",
-            fontWeight = FontWeight.Normal,
-            modifier = Modifier.fillMaxWidth(0.9f)
-        )
         Spacer(modifier = Modifier.size(10.dp))
-        NotDoTextField(text = text) {
+        NotDoTextField(
+            text = text,
+            labelText = "이메일",
+            hintText = "이메일을 입력해주세요.",
+            isError = isError,
+            errorMsg = "이메일 형식에 맞게 입력해주세요."
+        ) {
             text = it
         }
     }
-    Column(modifier = Modifier.fillMaxHeight(), verticalArrangement = Arrangement.Bottom) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Bottom,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Row(modifier = Modifier
             .notDoClickable {
-
+                //TODO 로그인으로 이동 로직 작성
             }
         ) {
             NotDoFont.Caption1(text = "이미 낫두 회원이신가요? ")
             NotDoFont.Caption1(text = "로그인 하러가기", fontWeight = FontWeight.SemiBold)
         }
+        Spacer(modifier = Modifier.size(24.dp))
         NotDoButton(text = "다음", isActivation = text != "") {
-            navController.navigate(NotDoRoutes.SignUp.EMAIL_INPUT_SCREEN)
+            isError = text.isEmailPattern()
+            if (!isError) {
+                //TODO 이메일 인증페이지로 이동 로직 작성
+            }
         }
-        Spacer(modifier = Modifier.fillMaxHeight(0.1f))
+        Spacer(modifier = Modifier.fillMaxHeight(0.06f))
     }
 }
