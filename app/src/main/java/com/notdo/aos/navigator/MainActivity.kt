0f462b67d0f8b4a033ee872a34382fdc08499fd4
khs3994 <z01039948003@gmail.com>
2023-04-15 16:55:25 +0900

    :bug: :: 이메일 인증 재전송시 시간이 0초면 타이머 작동 안하던 문제 해결

--- app/src/main/java/com/notdo/aos/navigator/MainActivity.kt
+++ app/src/main/java/com/notdo/aos/navigator/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.notdo.aos.navigator
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
--- feature-ui-signup/src/main/java/com/notdo/feature_ui_signup/ui/EmailCodeCheckScreen.kt
+++ feature-ui-signup/src/main/java/com/notdo/feature_ui_signup/ui/EmailCodeCheckScreen.kt
@@ -1,5 +1,6 @@
 package com.notdo.feature_ui_signup.ui
 
+import android.os.CountDownTimer
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.*
@@ -13,36 +14,28 @@ import com.notdo.util_ui.component.EmailCodeCheckTextField
 import com.notdo.util_ui.component.GreetingsNotDoView
 import com.notdo.util_ui.component.NormalHeader
 import com.notdo.util_ui.component.NotDoButton
-import kotlinx.coroutines.delay
 
 @Composable
 fun EmailCodeCheckScreen(navController: NavController) {
-    var emailCheckCode by remember {
-        mutableStateOf("")
-    }
+    var emailCheckCode by remember { mutableStateOf("") }
+    var isError by remember { mutableStateOf(false) }
+    var errorMsg by remember { mutableStateOf("") }
+    var time by remember { mutableStateOf(301) }
 
-    var isError by remember {
-        mutableStateOf(false)
-    }
+    var countDownTimer: CountDownTimer by remember {
+        mutableStateOf(
+            object : CountDownTimer((time * 1000).toLong(), 1000) {
+                override fun onTick(millisUntilFinished: Long) {
+                    time = (millisUntilFinished / 1000).toInt()
+                }
 
-    var errorMsg by remember {
-        mutableStateOf("")
-    }
-
-    var time by remember {
-        mutableStateOf(300)
-    }
-
-    LaunchedEffect("email-code-check-time") {
-        while (true) {
-            delay(1000)
-            time--
-            if (time == 0) {
-                isError = true
-                errorMsg = "인증번호가 일치하지 않아요."
-                break
-            }
-        }
+                override fun onFinish() {
+                    isError = true
+                    errorMsg = "인증번호 유효시간이 만료었어요."
+                    time = 0
+                }
+            }.start()
+        )
     }
 
     Column(
@@ -64,8 +57,19 @@ fun EmailCodeCheckScreen(navController: NavController) {
             time = time,
             errorMsg = errorMsg,
             onReSendButtonClick = {
-                //TODO 이메일 재전송 요청 로직
+                countDownTimer.cancel()
                 time = 300
+                countDownTimer = object : CountDownTimer((time * 1000).toLong(), 1000) {
+                    override fun onTick(millisUntilFinished: Long) {
+                        time = (millisUntilFinished / 1000).toInt()
+                    }
+
+                    override fun onFinish() {
+                        isError = true
+                        errorMsg = "인증번호 유효시간이 만료었어요."
+                        time = 0
+                    }
+                }.start()
             }
         ) {
             emailCheckCode = it
@@ -80,6 +84,7 @@ fun EmailCodeCheckScreen(navController: NavController) {
         NotDoButton(text = "인증", isActivation = emailCheckCode != "") {
             if (!isError) {
                 //TODO 서버통신으로 코드 인증 로직
+                countDownTimer.cancel()
                 navController.navigate(NotDoRoutes.SignUp.PASSWORD_INPUT_SCREEN)
             }
         }
