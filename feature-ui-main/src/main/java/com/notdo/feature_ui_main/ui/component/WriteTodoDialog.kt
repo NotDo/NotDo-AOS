6d5db824124dfc4a080d6da30535f6bdff86bf1c
khs3994 <z01039948003@gmail.com>
2023-04-29 00:18:51 +0900

    :lipstick: :: Todo완성

--- feature-ui-main/src/main/java/com/notdo/feature_ui_main/ui/component/WriteTodoDialog.kt
+++ feature-ui-main/src/main/java/com/notdo/feature_ui_main/ui/component/WriteTodoDialog.kt
@@ -31,7 +31,7 @@ fun TodoInputDialog(
         ) {
             Column(
                 modifier = Modifier
-                    .fillMaxWidth(0.9f)
+                    .fillMaxWidth(0.85f)
                     .fillMaxHeight(0.51f)
                     .clip(RoundedCornerShape(10.dp))
                     .background(NotDoColor.White),
