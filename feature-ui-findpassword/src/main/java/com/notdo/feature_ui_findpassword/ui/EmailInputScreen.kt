1340f0184828bab4e07bfdae5c51fee2fd08291c
khs3994 <z01039948003@gmail.com>
2023-05-08 16:14:18 +0900

    :sparkles: :: Screen파일들 생성 및 navigator제작

---
+++ feature-ui-findpassword/src/main/java/com/notdo/feature_ui_findpassword/ui/EmailInputScreen.kt
@@ -0,0 +1,11 @@
+package com.notdo.feature_ui_findpassword.ui
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun EmailInputScreen(
+    navController: NavController
+) {
+
+}
